8ff989f370cc2f627088c8aeb4e3b1c9a177d3dc
juan <machucajuangabriel@gmail.com>
2021-03-04 09:24:48 -0300

    Dropdown Menu

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,7 +10,13 @@ class _InputPageState extends State<InputPage> {
   
   String _nombre = '';
   String _email = '';
+  String _password = '';
+  String _fecha = '';
   
+  String _menuSeleccionado = 'Primera';
+  List<String> _menuopciones = ['Primera', 'Segunda', 'Tercera', 'Cuarta'];
+  TextEditingController _inputFieldDateController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +30,11 @@ class _InputPageState extends State<InputPage> {
           Divider(),
           _crearEmail(),
           Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearFecha( context ),
+          Divider(),
+          _crearDropdown(),
           _mostrarValor()
 
         ],
@@ -75,14 +86,108 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _crearPassword(){
+    return TextField(
+      keyboardType: TextInputType.visiblePassword,
+      decoration: InputDecoration(
+        hintText: 'Escribe tu contraseña . . .',
+        labelText: 'Contraseña',
+        suffixIcon: Icon( Icons.lock_open ),
+        icon: Icon( Icons.lock_rounded)
+      ),
+      onChanged: (valor){
+        setState(() {
+          
+          _password = valor;
+          
+        });
+      },
+    );
+  }
+
+  Widget _crearFecha( BuildContext context ) {
+    return TextField(
+      controller: _inputFieldDateController,
+      enableInteractiveSelection: false,
+      decoration: InputDecoration(
+        hintText: 'Fecha de Nacimiento . . .',
+        labelText: 'Fecha',
+        suffixIcon: Icon( Icons.perm_contact_calendar ),
+        icon: Icon( Icons.calendar_today )
+      ),
+      onTap: (){
+
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate( context );
+
+      },
+    );
+  }
+
+
   Widget _mostrarValor() {
 
     return ListTile(
       title: Text('El valor ingresado es: '+_nombre),
-      subtitle: Text('Email: '+_email),
+      subtitle: Text('Email: '+_email+' Contraseña: '+_password+' Fecha: '+_fecha),
     );
 
   }
 
+  void _selectDate(BuildContext context) async {
+
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(1990),
+      lastDate: new DateTime(2025)
+    ); 
+
+    if( picked != null ){
+      setState((){
+
+        _fecha = picked.toString();
+        _inputFieldDateController.text = _fecha;
+
+      });
+    }
+
+  }
+
+
+  List<DropdownMenuItem<String>> getOpcionesDropdown(){
+
+    List<DropdownMenuItem<String>> lista = [];
+
+    _menuopciones.forEach( (menuopt) {
+
+      lista.add( DropdownMenuItem(
+        child: Text( menuopt ),
+        value: menuopt, 
+      ));
+
+    });
+
+    return lista;
+
+  }
+
+
+  Widget _crearDropdown() {
+
+    return DropdownButton(     
+      value: _menuSeleccionado,
+      items: getOpcionesDropdown(),
+      onChanged: (opt){
+        setState(() {
+          
+          _menuSeleccionado = opt;
+          print(_menuSeleccionado);
+        });
+      },
+      
+    );
+
+  }
   
-}
+} 
